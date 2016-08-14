71a11cd1f774328c83edfc7cb5763246fd9319f6
Kenji ITO <mystralkk@gmail.com>
2016-08-14 13:01:53 +0900

    Import v1.1.4

--- configuration_validation.php
+++ configuration_validation.php
@@ -33,12 +33,12 @@ if (stripos($_SERVER['PHP_SELF'], basename(__FILE__)) !== FALSE) {
 require_once dirname(__FILE__) . '/install_defaults.php';
 
 // Open Graph Protocol Plugin Main Settings
-$_CONF_VALIDATE['ogp']['fb_user_ids']        = array('rule' => 'decimal');
-$_CONF_VALIDATE['ogp']['fb_app_id']          = array('rule' => 'decimal');
+$_CONF_VALIDATE['ogp']['fb_user_ids']        = array('rule' => 'numeric');
+$_CONF_VALIDATE['ogp']['fb_app_id']          = array('rule' => 'numeric');
 $_CONF_VALIDATE['ogp']['fb_default_img_url'] = array('rule' => 'stringOrEmpty');
-$_CONF_VALIDATE['ogp']['like_width']         = array('rule' => 'decimal');
-$_CONF_VALIDATE['ogp']['comments_num_posts'] = array('rule' => 'decimal');
-$_CONF_VALIDATE['ogp']['comments_width']     = array('rule' => 'decimal');
+$_CONF_VALIDATE['ogp']['like_width']         = array('rule' => 'numeric');
+$_CONF_VALIDATE['ogp']['comments_num_posts'] = array('rule' => 'numeric');
+$_CONF_VALIDATE['ogp']['comments_width']     = array('rule' => 'numeric');
 $_CONF_VALIDATE['ogp']['types']              = array(
 	'rule' => array('inList', $_OGP_DEFAULT['types'])
 );
